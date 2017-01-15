40ea4ff064c0fe9419fc41550a48856d9cf91a07
佐藤健太郎 <kanazaiku@yahoo.co.jp>
2017-01-16 00:49:05 +0900

    teststorm 新規追加

---
+++ teststorm/src/main/java/jp/co/teststorm/ExclamationBolt.java
@@ -0,0 +1,42 @@
+package jp.co.teststorm;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * [Class Description]
+ *
+ * @author Grant Henke
+ * @since 12/1/12
+ */
+public class ExclamationBolt extends BaseRichBolt {
+    private static final long serialVersionUID = -3086054753171924795L;
+
+    OutputCollector _collector;
+    
+    Logger LOGGER = LoggerFactory.getLogger(ExclamationBolt.class);
+
+    public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+        _collector = collector;
+    }
+
+    public void execute(Tuple tuple) {
+    	LOGGER.info("message : " + tuple.getStringByField("word"));
+        _collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
+        _collector.ack(tuple);
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("word"));
+    }
+}
---
+++ teststorm/src/test/java/teststorm/TestStorm.java
@@ -0,0 +1,13 @@
+package teststorm;
+
+import storm.contrib.spring.topology.LocalTopologySubmitter;
+
+public class TestStorm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] array = {"spring-context.xml", "exclamationTopologySubmission","60000"};
+		LocalTopologySubmitter.main(array);
+	}
+
+}
